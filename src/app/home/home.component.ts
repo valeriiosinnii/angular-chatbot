9a0bce4cd6d398d49b676ca17d056522483d8a13
valeriiceo <valeriiceo@gmail.com>
2024-03-29 20:25:32 +0100

    new feature added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
+import { Contacts } from '../contacts';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  myContact : Contacts = {
+    id: 0,
+    name: "Valerii"
+  }
+
+  contactList : Contacts[] = [
+    {id: 1,
+    name: "Pyshik"},
+    {id: 2,
+    name: "Bogdan"},
+    {id: 3,
+    name: "Marco"}
+  ]
+}
